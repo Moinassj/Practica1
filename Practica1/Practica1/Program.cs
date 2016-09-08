534198e7270849f6ba93ba7d4195a502c09e7d7f
moyrene84 <moyrene84@gmail.com>
2016-09-07 18:38:47 -0700

    Practica1

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Numero = 1;
+            while (Numero <= 100)
+            {
+                Console.Write(Numero);
+                Console.Write(" - ");
+                Numero = Numero + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
